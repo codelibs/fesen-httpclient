ea8eb851cf4d5dba30be05931b309da67a971e5f
Shinsuke Sugaya <shinsuke@apache.org>
2024-07-12 08:54:51 +0900

    elasticsearch 8.14.3

--- src/test/java/org/codelibs/fesen/client/Elasticsearch8ClientTest.java
+++ src/test/java/org/codelibs/fesen/client/Elasticsearch8ClientTest.java
@@ -110,7 +110,7 @@ import org.testcontainers.utility.DockerImageName;
 class Elasticsearch8ClientTest {
     static final Logger logger = Logger.getLogger(Elasticsearch8ClientTest.class.getName());
 
-    static final String version = "8.14.1";
+    static final String version = "8.14.3";
 
     static final String imageTag = "docker.elastic.co/elasticsearch/elasticsearch:" + version;
 
